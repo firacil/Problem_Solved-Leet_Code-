687f3c905247a266e5afa20932b0779515e0eb54
Firaol Mekbib <firaolmekbib511@gmail.com>
2025-03-08 18:47:44 +0300

    Delete DataStructures directory

--- DataStructures/Array/removeDuplicates.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        j = 0 # track unique elements
-
-        for i in range(1, len(nums)):
-            if nums[i] != nums[j]:
-                j += 1
-                nums[j] = nums[i]
-        return j + 1
